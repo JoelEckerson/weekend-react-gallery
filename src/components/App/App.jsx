9ee40e9f745e0d1fb938b8a1827a34a3aebcf2d7
Joel Eckerson <joelaeckerson@gmail.com>
2021-03-27 12:56:38 -0500

    working through GET and PUT

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import {useEffect, useState} from 'react';
+import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
 
@@ -11,28 +12,36 @@ useEffect( ()=>{
   getGallery();
 }, []);
 
-let getGallery = () => {
-  console.log('in getGallery')
-  axios.get('/gallery').then( (response )=>{
-    console.log( 'back from GET with:', response.data )
-    setGallery( response.data );
-  }).catch((err)=>{
-    console.log(err);
-    alert( 'your GET says no' );
-  }) 
-}
+    let getGallery = () => {
+      console.log('in getGallery')
+      axios.get('/gallery')
+      .then( (response )=>{
+        console.log( 'back from GET with:', response.data )
+        setGallery( response.data );
+      }).catch((err)=>{
+        console.log(err);
+        alert( 'your GET says no' );
+      }) 
+    }
+
+    const addLike = ( id ) => {
+      console.log( 'in addLike', id);
+      axios.put( '/gallery/like/' + id)
+      .then( response => {
+        console.log( 'back from PUT with:', response );
+        getGallery();
+      }).catch( err =>{
+        console.log( err );
+        alert( 'Your images do not LIKE this' );
+      })
+    }
 
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <p>{JSON.stringify(gallery)}</p>
-          <ul>
-            { gallery.map( ( item, index ) => <li key={index}> 
-            <img src={item.path}></img> {item.description} {item.likes} </li>)}
-          </ul>
+        <GalleryList gallery={ gallery } addLike = { addLike } />
       </div>
     );
 }
